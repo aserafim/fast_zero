fbb2a02850cab25ab9d08873aa866e1cc44b84ed
aserafim <correia.alefe@gmail.com>
2024-06-25 19:32:44 -0300

    Finalizados os verbos do CRUD com testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -25,3 +25,47 @@ def test_creat_user(client):
         'email': 'teste1@teste.com',
         'id': 1,
     }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Teste1',
+                'email': 'teste1@teste.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': 'teste1pass',
+            'username': 'testusername2',
+            'email': 'test@test.com',
+            'id': 1,
+        },
+    )
+
+    assert response.json() == {
+        'username': 'testusername2',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_not_found(client):
+    return 0
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'Usuário removido'}
